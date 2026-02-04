13ec454cc5c8dc10eae3cacf9ce48c6121a84faa
Ericcost <eric.costerousse@laposte.net>
2026-02-04 14:17:51 +0100

    Feature: Third commit

--- average.py
+++ average.py
@@ -4,4 +4,6 @@
 #         return 0
 #     return sum(liste) / len(liste)
 def moyenne(liste):
-    return sum(liste)  # bug : ne divise pas par len(liste)
+    if not liste:
+        return 0
+    return sum(liste) / len(liste)
