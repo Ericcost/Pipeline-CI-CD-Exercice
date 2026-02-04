355f582131c3822a5f2c183fd998a892f6e9925e
Ericcost <eric.costerousse@laposte.net>
2026-02-04 14:02:45 +0100

    Ajout job.py, mymath.py et tests

--- average.py
+++ average.py
@@ -1,5 +1,7 @@
 # mymath.py
+# def moyenne(liste):
+#     if not liste:
+#         return 0
+#     return sum(liste) / len(liste)
 def moyenne(liste):
-    if not liste:
-        return 0
-    return sum(liste) / len(liste)
+    return sum(liste)  # bug : ne divise pas par len(liste)
